a89ac249209ec3583439545199e4f9a136883ac0
andorxornot <andorxornot@gmail.com>
2020-08-12 18:43:22 +0300

    Added mk file for win

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxMurka
+	ADDON_DESCRIPTION = Addon for rich UI
+	ADDON_AUTHOR = Lyosha
+	ADDON_TAGS = "rich UI" "immediate mode" 
+	ADDON_URL = https://github.com/Kiberchaika/ofxMurka
+
+vs:
+	ADDON_SOURCES_EXCLUDE = src/mac/%
+	ADDON_INCLUDES_EXCLUDE = src/mac/%
